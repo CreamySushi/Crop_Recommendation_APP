8f5042e92f155a97af69804b3e3469e83d383f3d
CreamySushi <johnaaronflavien@gmail.com>
2026-03-26 10:48:15 +0800

    fix model path

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ CORS(app)
 
 # ------------------------ FILE CONFIGURATION ------------------------
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-MODEL_PATH = os.path.join(BASE_DIR, 'models', 'cr_xgbrfclassifier.model.pkl')
+MODEL_PATH = os.path.join(BASE_DIR, 'models', 'cr_xgbrfclassifier_model.pkl')
 ENCODER_PATH = os.path.join(BASE_DIR, 'models', 'label_encoder.pkl')
 
 
