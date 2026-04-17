e6fd0fb14bbea960209cfa7d05638a6f304d4427
CreamySushi <johnaaronflavien@gmail.com>
2026-04-18 03:32:54 +0800

    Update API and model files

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ CORS(app)
 
 # ------------------------ FILE CONFIGURATION ------------------------
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-MODEL_PATH = os.path.join(BASE_DIR, 'models', 'crop_recommendation_model.pkl')
+MODEL_PATH = os.path.join(BASE_DIR, 'models', 'cr_xgbrfclassifier_model.pkl')
 ENCODER_PATH = os.path.join(BASE_DIR, 'models', 'label_encoder.pkl')
 
 # Account Key (Secret File)
