8186eda75c1216a86811e5c16b052759d20aab22
CreamySushi <johnaaronflavien@gmail.com>
2026-04-23 22:54:57 +0800

    Fix Railway startup: add json import and use dynamic PORT

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
 import hmac
+import json
 import joblib
 import pandas as pd
 import firebase_admin
@@ -287,5 +288,6 @@ def get_crop_requirements(crop_name):
     }), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
     
